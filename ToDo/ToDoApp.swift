c330892060f24aba1ea587cf122454fc8ab1b481
volt901 <89735853+volt901@users.noreply.github.com>
2023-05-05 00:09:16 +0500

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Владимир Ширяев on 05.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
